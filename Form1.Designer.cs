f2e4b5f1578ec33e9f205a2a5c50dc8221deff93
NhanTruong <truongchinhan2010@gmail.com>
2023-03-13 17:38:48 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Text_To_Speech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 507);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(789, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(789, 507);
+            this.splitContainer1.SplitterDistance = 162;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.txtContent);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.btnExport);
+            this.splitContainer2.Panel2.Controls.Add(this.btnStop);
+            this.splitContainer2.Panel2.Controls.Add(this.btnPause);
+            this.splitContainer2.Panel2.Controls.Add(this.btnResume);
+            this.splitContainer2.Panel2.Controls.Add(this.btnSpeak);
+            this.splitContainer2.Size = new System.Drawing.Size(623, 507);
+            this.splitContainer2.SplitterDistance = 470;
+            this.splitContainer2.TabIndex = 2;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtContent.Location = new System.Drawing.Point(0, 0);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(623, 470);
+            this.txtContent.TabIndex = 2;
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSpeak.Location = new System.Drawing.Point(0, 0);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(118, 33);
+            this.btnSpeak.TabIndex = 0;
+            this.btnSpeak.Text = "Speak";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnResume.Location = new System.Drawing.Point(118, 0);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(109, 33);
+            this.btnResume.TabIndex = 1;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnPause.Location = new System.Drawing.Point(227, 0);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(100, 33);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnStop.Location = new System.Drawing.Point(327, 0);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(105, 33);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExport.Location = new System.Drawing.Point(531, 0);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(92, 33);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Export...";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 529);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button btnSpeak;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SpeechLib;
+
+namespace Text_To_Speech
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer speechSynthesizerObj;
+       
+        ISpeechAudio speechAudio;
+        public Form1()
+        {
+            InitializeComponent();
+            speechSynthesizerObj = new SpeechSynthesizer();
+            btnResume.Enabled = false;
+            btnPause.Enabled = false;
+            btnStop.Enabled = false;
+
+           
+
+        }
+
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            //Disposes the SpeechSynthesizer object   
+            speechSynthesizerObj.Dispose();
+            if (txtContent.Text != "")
+            {
+                speechSynthesizerObj = new SpeechSynthesizer();
+                //Asynchronously speaks the contents present in RichTextBox1   
+                speechSynthesizerObj.SpeakAsync(txtContent.Text);
+                btnPause.Enabled = true;
+                btnStop.Enabled = true;
+            }
+
+            
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (speechSynthesizerObj != null)
+            {
+                //Gets the current speaking state of the SpeechSynthesizer object.   
+                if (speechSynthesizerObj.State == SynthesizerState.Speaking)
+                {
+                    //Pauses the SpeechSynthesizer object.   
+                    speechSynthesizerObj.Pause();
+                    btnResume.Enabled = true;
+                    btnSpeak.Enabled = false;
+                }
+            }
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            if (speechSynthesizerObj != null)
+            {
+                if (speechSynthesizerObj.State == SynthesizerState.Paused)
+                {
+                    //Resumes the SpeechSynthesizer object after it has been paused.   
+                    speechSynthesizerObj.Resume();
+                    btnResume.Enabled = false;
+                    btnSpeak.Enabled = true;
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (speechSynthesizerObj != null)
+            {
+                //Disposes the SpeechSynthesizer object   
+                speechSynthesizerObj.Dispose();
+                btnSpeak.Enabled = true;
+                btnResume.Enabled = false;
+                btnPause.Enabled = false;
+                btnStop.Enabled = false;
+            }
+        }
+
+
+
+    }
+}
